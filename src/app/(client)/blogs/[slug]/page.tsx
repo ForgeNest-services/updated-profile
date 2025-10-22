d1c496b7d1cc64cde0f74257c057b4e4c7d72df0
Nishant <nishantchy1234@gmail.com>
2025-10-21 23:41:27 -0700

    start blog pages

---
+++ src/app/(client)/blogs/[slug]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BlogetailPage() {
+  return <div>page</div>;
+}
--- src/lib/constants/navlinks.ts
+++ src/lib/constants/navlinks.ts
@@ -7,5 +7,6 @@ export const navLinks: NavLink[] = [
   { label: "Home", href: "/" },
   { label: "About", href: "/about-us" },
   { label: "Services", href: "/our-services" },
+  { label: "Blogs", href: "/blogs" },
   { label: "Contact", href: "/contact-us" },
 ];
